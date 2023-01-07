fad67f2c1829f327904a2c85b2d68ba43a27f325
Klyrasel <KaiLyrasel@outlook.com>
2023-01-07 10:49:03 -0500

    imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect, useState, Fragment } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
