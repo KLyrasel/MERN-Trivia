1d9b0de14379ee71b5439c6820c0ef6c0812833e
Klyrasel <KaiLyrasel@outlook.com>
2023-01-13 16:47:13 -0500

    updated navbar for material ui and created components

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Game from './components/Game'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Scoreboard from './components/Scoreboard'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>TRIVIA APP</h1>
 
+    <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/game' element={<Game />} />
+          <Route path='/' element={<Home/>}/>
+          <Route path='/game' element={<Game/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+          <Route path='/scoreboard' element={<Scoreboard/>}/>
+          <Route path='/navbar' element={<Navbar/>}/>
         </Routes>
       </Router>
     </div>
+
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import react from "react"
+import * as Mui from "@mui/material"
+import { Link } from "react-router-dom"
+
+
+function Navbar() {
+    return (
+      <Mui.Box sx={{ flexGrow: 1 }}>
+        <Mui.AppBar position="static">
+          <Mui.Toolbar>
+            <Mui.IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+            </Mui.IconButton>
+            <Mui.Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link to="/">
+              Home
+              </Link>
+            </Mui.Typography>
+            <Mui.Button color="inherit">Login</Mui.Button>
+          </Mui.Toolbar>
+        </Mui.AppBar>
+      </Mui.Box>
+    );
+  }
+
+export default Navbar;
