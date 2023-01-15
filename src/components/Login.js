ab3cde302290475ba78948ae53fd625d6c8f2a60
Klyrasel <KaiLyrasel@outlook.com>
2023-01-14 19:26:09 -0500

    login and signup pages complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from "react"
 import * as Mui from "@mui/material"
+import { Link } from "react-router-dom"
 
 function Login() {
     return(
@@ -25,7 +26,8 @@ function Login() {
             pb: 0,
             m: 0,
             pr:20,}}>
-            Login</Mui.FormLabel>
+            Login
+            </Mui.FormLabel>
         <Mui.Box component="form" sx={{ '& .MuiTextField-root': { m: 1, width: '30ch' },
          }}
          noValidate
@@ -38,7 +40,7 @@ function Login() {
           defaultValue="Username"/>
         </div>
         </Mui.Box>
-        <Mui.Box component="form" sx={{ '& .MuiTextField-root': { m: 1, width: '30ch', pb: 12, },
+        <Mui.Box component="form" sx={{ '& .MuiTextField-root': { m: 1, width: '30ch', pb: 5, },
         }}
          noValidate
         autoComplete="off">
@@ -50,8 +52,20 @@ function Login() {
          defaultValue="Password"/>
         </div>
         </Mui.Box>
-        <Mui.Box>
-            
+        <Mui.Box display="flex" justifyContent="space-evenly" paddingBottom="30px" margin="0" paddingTop="0">
+         <Mui.Button
+            type="submit"
+            variant="contained">
+            {'Login'}
+          </Mui.Button>
+
+          <Mui.Button
+            type="submit"
+            variant="contained">
+            { <Link to="/Signup" style={{ textDecoration: 'none' , color: 'inherit',}}>
+                 Sign up
+            </Link>}
+          </Mui.Button>   
         </Mui.Box>
         </Mui.Box>
     </div>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,86 @@
 import React from 'react'
+import * as Mui from "@mui/material"
+import { Link } from "react-router-dom"
 
 function Signup() {
-    return (
-        <div>
-            sign up
+        return(
+            <div>
+            <Mui.Box sx={{
+                display: 'flex',
+                 flexDirection: 'column',
+                justifyContent: 'space-evenly',
+                margin: '50px auto',
+                padding: '50px',
+                width: '500px',
+                height: '400px',
+                boxShadow: '0px 0px 20px 0px grey',
+                borderRadius: '30px'
+            }}>
+            <Mui.FormLabel id='login' sx={{
+                color: "rgb(0,0,0)",
+                fontFamily: 'Helvetica',
+                fontWeight: 700,
+                fontSize: 30,
+                letterSpacing: '1px',
+                pt:8,
+                pb: 0,
+                m: 0,
+                pr:17,}}>
+                Sign Up
+                </Mui.FormLabel>
+                <Mui.Box component="form" sx={{ '& .MuiTextField-root': { m: 1, width: '30ch' },}}
+                noValidate
+                autoComplete="off">
+               <div>
+                <Mui.TextField
+                 required
+                 id="outlined-required"
+                 label="Required"
+                 defaultValue="Username"/>
+                </div>
+                </Mui.Box>
+            <Mui.Box component="form" sx={{ '& .MuiTextField-root': { m: 1, width: '30ch' },
+             }}
+             noValidate
+            autoComplete="off">
+            <div>
+            <Mui.TextField
+              required
+              id="outlined-required"
+              label="Required"
+              defaultValue="E-mail Address"/>
+            </div>
+            </Mui.Box>
+            <Mui.Box component="form" sx={{ '& .MuiTextField-root': { mt: 1, mb: 0, width: '30ch', pb: 0, },
+            }}
+             noValidate
+            autoComplete="off">
+            <div>
+            <Mui.TextField
+             required
+             id="outlined-required"
+             label="Required"
+             defaultValue="Password"/>
+            </div>
+            </Mui.Box>
+            <div>
+                <h5>Already have an account?  { <Link to="/Login" style={{ textDecoration: 'none' , color: 'inherit',}}>
+                Sign in!
+                </Link>} 
+                 </h5>
+            </div>
+            <Mui.Box display="flex" justifyContent="space-evenly" paddingBottom="30px" margin="0" paddingTop="0">
+              <Mui.Button
+                type="submit"
+                variant="contained">
+                { <Link to="/Signup" style={{ textDecoration: 'none' , color: 'inherit',}}>
+                     Sign up
+                </Link>}
+              </Mui.Button>   
+            </Mui.Box>
+            </Mui.Box>
         </div>
-    )
+        )
 }
 
 export default Signup
